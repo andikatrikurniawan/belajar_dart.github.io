883598dd1fa8e417f8a9760f7c2d9522132570c4
andikatrikurniawan <andikatrikurniawan199@gmail.com>
2026-01-26 23:59:01 +0700

    mencoba study kasus persegi

--- bin/input.dart
+++ bin/input.dart
@@ -18,6 +18,22 @@ void main (){
 
   stdout.write('Masukan umur Anda: ');
   var umurInput = num.parse(stdin.readLineSync()!);
-  print(  'Halo, Nama Saya $nama, Umur Saya $umurInput Tahun, Selamat datang di Dart programming language.');
+  print(  'Halo, Nama Saya $nama, Umur Saya $umurInput Tahun, Selamat datang di Dart programming language.\n');
+
+  print('========= Mencoba belajar Study Kasus ===================');
+
+  stdout.write('Masukkan panjang sisi persegi: ');
+  var panjangsisi = num.parse(stdin.readLineSync()!);
+  stdout.write('Masukkan lebar sisi persegi: ');
+  var lebarsisi = num.parse(stdin.readLineSync()!);
+  var luasPersegi = panjangsisi * lebarsisi;
+  print('Luas persegi dengan panjang sisi $panjangsisi cm dan lebar persegi $lebarsisi cm,\nmaka hasil dari luas persegi adalah :$luasPersegi cm persegi.\n');
+
+  print('========= Menghitung Volume ===================');
+  stdout.write('Masukkan panjang balok: ');
+  var panjangBalok = num.parse(stdin.readLineSync()!);
+  var volumeBalok = panjangBalok * luasPersegi;
+  print('========= Hasil Volume Balok ===================');
+  print('maka hasil dari volume balok adalah : $volumeBalok cm kubik.\n');
 
 }
