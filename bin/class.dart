182e2c1b5749fd4c7febfb29ad2408415924dbeb
andikatrikurniawan <andikatrikurniawan199@gmail.com>
2026-02-13 00:37:24 +0700

    Pelajaran udh sampe Redirecting Named Constructor

--- bin/class.dart
+++ bin/class.dart
@@ -38,13 +38,65 @@ class FirstClass{
   class Orange{
     int quantity = 0;
 
-   Orange operator +(Orange sor){
+   Orange operator *(Orange sor){
     var result = Orange();
-    result.quantity = quantity + sor.quantity;
+    result.quantity = quantity * sor.quantity;
     return result;
-   }
+    }
   }
+   
+  // Membuat constructor, Construktor tidak bisa dibuat lebih dari satu
+  class Engine{
+    var honda = "";
+    var jenis = "";
+    Engine(String motor, String merek){
+      honda = motor;
+      jenis = merek;
+    }
+    
+  }
+  // Membuat variable shadowing
+  class Mesin{
+    var honda = "Vario";
+    var jenis = "Honda";
+    Mesin(String honda, String jenis){
+      honda = honda;
+      jenis = jenis;
+    }
+  }
+  // Membuat this keyword
+  class Spesifikasi{
+    var merek = "Samsung";
+    var tahun = 2023;
 
+    Spesifikasi(String merek, int tahun){
+      this.merek = merek;
+      this.tahun = tahun;
+    }
+  }
+// initializing formal parameter
+  class Parameter{
+    var merek = "honda";
+    var tahun = "2023";
+
+    Parameter(this.merek, this.tahun);
+    //membuat named constructor bisa membuat constructor lebih dari satu
+    Parameter.code(this.merek, this.tahun){
+      print('Ini Adalah Named Constructor : $merek dan $tahun');
+    }
+    Parameter.hp(this.merek){
+      print('Ini Adalah Nama merek  : $merek');
+    }
+    
+    Parameter.jenis(this.tahun){
+      print('Ini Adalah Tahun Jenis : $tahun');
+    }
+    //Redirecting Constructor
+    Parameter.redirect(String merek) : this(merek, "No Tahun");
+    Parameter.fun(String tahun) :this("No Merek", tahun);
+    //Redirecting Named Constructor
+    Parameter.newConstruktor() : this.redirect("Indonesia");
+  }
 void main(){
   FirstClass first1 = FirstClass();
   print(first1.name.toUpperCase());
@@ -73,6 +125,27 @@ void main(){
   hitung1.quantity = 10;
   var hitung2 = Orange();
   hitung2.quantity = 20;
-  var value = hitung1.quantity + hitung2.quantity;   
+  var value = hitung1.quantity * hitung2.quantity;   
   print("Hasil Dari Perjumlahan adalah : $value");
+  print(" ");
+  var bengkel = Engine("Beat 2025", "Honda");
+  print(bengkel.honda); print(bengkel.jenis); print(" ");
+  var ba = Mesin("Mio","2023" );
+  print(ba.honda);print(ba.jenis); print(" "); 
+  var cs = Spesifikasi("Realme", 2026);
+  print(cs.merek); print(cs.tahun); print(" ");
+  var fp = Parameter("Baris", "300");
+  print(fp.merek); print(fp.tahun);print("");
+  Parameter.code("Nama Dulu", "1990"); print(" ");
+  Parameter.hp("xiaomi"); 
+  Parameter.jenis("2024");print(" ");
+  var rd = Parameter.redirect('Poco');
+  print(rd.merek);
+  print(rd.tahun); print(" ");
+  var rd1 = Parameter.fun('2025');
+  print(rd1.merek);
+  print(rd1.tahun); print("");
+  var rd2 = Parameter.newConstruktor();
+  print(rd2.merek);
+  print(rd2.tahun);     
 }
